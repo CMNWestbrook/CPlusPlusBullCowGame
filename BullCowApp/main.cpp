dd4ac5edf654f1a9f6e0f1c1e58ac784407c087e
CWestbrookSF <christina@cwestbrook.com>
2017-06-12 18:51:30 -0700

    cleaned and added code

--- BullCowApp/main.cpp
+++ BullCowApp/main.cpp
@@ -4,41 +4,46 @@
 using namespace std;
 
 void PrintIntro();
-string GuessPromptAndPrintBack();
+void PlayTheGame();
+string GuessPrompt();
 
-// num of turns is same as word length
-constexpr int WORD_LENGTH = 3;
+// num of turns is same as word length, put at top to change easily
+constexpr int NUM_OF_TURNS = 4;
 
-// beginning of app
+// Beginning of app!
 int main() 
 {
 	PrintIntro();
-
-	for (int i = 1; i <= WORD_LENGTH; i++)
-	{
-		GuessPromptAndPrintBack();
-		cout << endl;
-	}
-
+	PlayTheGame();
 	cout << endl;
-	return 0;
+	return 0; // gets user out of the app
 }
 
 // introduce the game with basic text
 void PrintIntro() {
 	cout << "Wecome one and all to the Bulls and Cows game!\n";
 	cout << "It's super fun, like so fun I can't even\n";
-	cout << "Guess the " << WORD_LENGTH << " character isogram!\n\n";
+	cout << "Guess the " << NUM_OF_TURNS << " character isogram!\n\n";
 	return;
 }
 
+
+void PlayTheGame()
+{
+	for (int i = 1; i <= NUM_OF_TURNS; i++) {
+		string Guess = GuessPrompt();
+		// repeat the guess back to the player by printing it
+		cout << "Your guess is the word " << Guess << endl;
+		cout << endl;
+	}
+}
+
+
 // prompt the player to make a guess
-string GuessPromptAndPrintBack() {
-	cout << "Enter a word that's " << WORD_LENGTH << " characters long: ";
+string GuessPrompt() {
+	cout << "Enter a word that's " << NUM_OF_TURNS << " characters long: ";
 	string Guess = "";
 	getline(cin, Guess);
 	// cin >> Guess;
-	// repeat the guess back to the player by printing it
-	cout << "Your guess is the word " << Guess << endl;
 	return Guess;
 }
